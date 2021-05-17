6dd52031c0c86c471ed7bb21d532fd7dbf851bcd
adityamulik <aditya.mulik@gmail.com>
2021-05-17 15:48:39 -0400

    removed delete feedback

--- src/routes/deleteFeedback.js
+++
@@ -1,9 +0,0 @@
-const deleteFeedback = {
-  method: 'delete',
-  path: '/api/employees/:employee_id/feedback/:feedback_id',
-  handler: async (req, res) => {
-    
-  }
-}
-
-export default deleteFeedback;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import addEmployee from './addEmployee';
 import addFeedback from './addFeedback';
 import deleteEmployee from './deleteEmployee';
-import deleteFeedback from './deleteFeedback';
 import employeeList from './employeeList';
 import getEmployee from './getEmployee';
 
@@ -9,7 +8,6 @@ export const routes = [
   addEmployee,
   addFeedback,
   deleteEmployee,
-  deleteFeedback,
   employeeList,
   getEmployee
 ];
